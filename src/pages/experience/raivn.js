465a9d7db82481309b7c773547bb9f8047660b11
Javon Hickmon <jj.hickmon@gmail.com>
2024-01-17 11:20:30 -0800

    Updated RAIVN abstract

--- src/pages/experience/raivn.js
+++ src/pages/experience/raivn.js
@@ -18,25 +18,34 @@ function Raivn() {
             <p>Within the RAIVN Lab I have:</p>
             <ul>
                 <li>
-                    Led the development of a novel method to utilize generative diffusion-based model
-                    ensembling to improve multimodal <b>fine-grained image classification</b> accuracy.
+                    Led the development of a novel method to utilize generative
+                    diffusion-based model ensembling to improve multimodal{" "}
+                    <b>fine-grained image classification</b> accuracy.
                     <ul>
-                        <li>We have already achieved accuracies higher than OpenAI's CLIP ViT-L/14 model on a variety of datasets,
-                            although the project is currently still in development.</li>
+                        <li>
+                            We have already achieved accuracies higher than
+                            OpenAI's CLIP ViT-L/14 model on a variety of
+                            datasets, although the project is currently still in
+                            development.
+                        </li>
                     </ul>
                 </li>
                 <li>
                     Researched methods to leverage the knowledge contained
-                    within <b>Large Language Models</b> to improve image classification
-                    for <b>Multimodal Machine Learning</b> models.
+                    within <b>Large Language Models</b> to improve image
+                    classification for <b>Multimodal Machine Learning</b>{" "}
+                    models.
                 </li>
                 <li>
-                    Wrote custom evaluation code, resulting in a <b>~15x evaluation speedup</b> while
-                    decreasing the misclassification rate of our system.
+                    Wrote custom evaluation code, resulting in a{" "}
+                    <b>~15x evaluation speedup</b> while decreasing the
+                    misclassification rate of our system.
                 </li>
                 <li>
-                    Independently wrote the implementation of <b>multimodal chain-of-thought</b> and showed
-                    consistently higher accuracies on a subset of images from the ImageNet dataset.
+                    Independently wrote the implementation of{" "}
+                    <b>multimodal chain-of-thought</b> and showed consistently
+                    higher accuracies on a subset of images from the ImageNet
+                    dataset.
                 </li>
             </ul>
             <p>
@@ -63,25 +72,23 @@ function Raivn() {
                 Multimodal Ensembling for Zero-Shot Image Classification
             </h4>
             <p>
-                Artificial intelligence has made significant progress in im-
-                age classification, an essential task for machine perception
-                to achieve human-level image understanding. Despite recent
-                advances in vision-language fields, multimodal image classi-
-                fication is still challenging, particularly for the following two
-                reasons. First, models with low capacity often suffer from un-
-                derfitting and thus underperform on fine-grained image clas-
-                sification. Second, it is important to ensure high-quality data
-                with rich cross-modal representations of each class, which is
-                often difficult to generate. Here, I utilize Ensemble Learn-
-                ing to reduce the impact of these issues on pre-trained mod-
-                els. By leveraging open-vocabulary models, I aim to create a
-                meta-model that combines the predictions of multiple open-
-                vocabulary multimodal models trained on different data to
-                create more robust predictions. In this work, I intend to im-
-                prove fine-grained classification accuracy without additional
-                training or fine-tuning by creating an ensemble of multimodal
-                models. This ensemble will better distinguish the differences
-                between similar classes while staying completely zero-shot.
+                Artificial intelligence has made significant progress in image
+                classification, an essential task for machine perception to
+                achieve human-level image understanding. Despite recent advances
+                in vision-language fields, multimodal image classification is
+                still challenging, particularly for the following two reasons.
+                First, models with low capacity often suffer from underfitting
+                and thus underperform on fine-grained image classification.
+                Second, it is important to ensure high-quality data with rich
+                cross-modal representations of each class, which is often
+                difficult to generate. Here, we utilize Ensemble Learning to
+                reduce the impact of these issues on pre-trained models. We aim
+                to create a meta-model that combines the predictions of multiple
+                open-vocabulary multimodal models trained on different data to
+                create more robust and accurate predictions. By utilizing
+                ensemble learning for multimodal models, we will achieve higher
+                prediction accuracies without any additional training or
+                fine-tuning, meaning that this method is completely zero-shot.
                 <br />
                 <br />
                 This work has been accepted for presentation at the{" "}
@@ -124,38 +131,40 @@ function Raivn() {
                 modality of data which will result in fairer and much more
                 representative systems.
                 <br />
-                <br />
-                I presented this work at the{" "}
+                <br />I presented this work at the{" "}
                 <a
                     target='_blank'
                     rel='noopener noreferrer'
                     href='https://starscelebration.org/'>
                     2022 STARS Celebration
-                </a>
-                {" "}and the{" "}
+                </a>{" "}
+                and the{" "}
                 <a
                     target='_blank'
                     rel='noopener noreferrer'
                     href='https://www.aap.ucla.edu/mcnair/'>
-                    2023 UCLA National McNair Conference</a>.
+                    2023 UCLA National McNair Conference
+                </a>
+                .
                 <br />
                 <ul>
-                    <li>To view my presentation on this work, check out{" "}
-                    <a
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        href='https://docs.google.com/presentation/d/1UZI9BCWsTV9rFBdLbpWmfuu9cpbSSKb-yD296iUBC54/edit?usp=sharing'>
-                        this link
-                    </a>
+                    <li>
+                        To view my presentation on this work, check out{" "}
+                        <a
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            href='https://docs.google.com/presentation/d/1UZI9BCWsTV9rFBdLbpWmfuu9cpbSSKb-yD296iUBC54/edit?usp=sharing'>
+                            this link
+                        </a>
                     </li>
                     <li>
-                    You can find my poster for this project{" "}
-                    <a
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        href='https://docs.google.com/presentation/d/1_AUv1MG6D4UZlUQ866kB8j0U6T11c8Fo_nwKFAqsn_c/edit?usp=sharing'>
-                        here
-                    </a>
+                        You can find my poster for this project{" "}
+                        <a
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            href='https://docs.google.com/presentation/d/1_AUv1MG6D4UZlUQ866kB8j0U6T11c8Fo_nwKFAqsn_c/edit?usp=sharing'>
+                            here
+                        </a>
                     </li>
                 </ul>
             </p>
